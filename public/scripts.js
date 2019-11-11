9bf9881a0aced8c7080f688425b7a4d13b1a9881
Aashish <snipertechstuff@gmail.com>
2019-11-11 20:19:06 +0530

    Removed event listener

--- public/scripts.js
+++ public/scripts.js
@@ -1,28 +1,32 @@
 function changeTheme() {
-    editor.setTheme("ace/theme/theme-twilight");
-  }
-  function changeCTheme() {
-    editor.setTheme("ace/theme/clouds");
-  }
-  var editor = ace.edit("editor");
   editor.setTheme("ace/theme/theme-twilight");
-  editor.session.setMode("ace/mode/javascript");
+}
+function changeCTheme() {
+  editor.setTheme("ace/theme/clouds");
+}
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/theme-twilight");
+editor.session.setMode("ace/mode/javascript");
 
-  const socket = io("http://localhost:8888");
-  console.log(socket);
+const socket = io("http://localhost:8888");
+console.log(socket);
 
-  document.querySelector("#editor").addEventListener("keypress", e => {
-    //e.preventDefault();
-    console.log(editor.getValue());
-    var code = editor.getValue();
-    socket.emit("updSv", { code: code });
-  });
-  document.querySelector("#editor").addEventListener("paste", e => {
-    e.preventDefault();
-    var code = editor.getValue();
-    socket.emit("updSv", { code });
-  });
+//   document.querySelector("#editor").addEventListener("keypress", e => {
+//     //e.preventDefault();
+//     console.log(editor.getValue());
+//     var code = editor.getValue();
+//     socket.emit("updSv", { code: code });
+//   });
+//   document.querySelector("#editor").addEventListener("paste", e => {
+//     e.preventDefault();
+//     var code = editor.getValue();
+//     socket.emit("updSv", { code });
+//   });
+editor.on("change", e => {
+  var code = editor.getValue();
+  socket.emit("updSv", { code: code });
+});
 
-  socket.on("updCl", data => {
-    editor.setValue(data.abc + " ");
-  });
+socket.on("updCl", data => {
+  editor.setValue(data.abc + " ");
+});
